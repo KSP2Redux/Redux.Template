b747c83af81242aed196440b69efcdcfd4a7e5f7
Lexi <arallen3660@gmail.com>
2025-07-25 08:52:25 -0400

    Update gitignore and remove old mod template stuff

--- Assets/Scripts/TestPlugin.cs
+++
@@ -1,9 +0,0 @@
-using SpaceWarp.API.Mods;
-
-public class TestPlugin : MonoBehaviourMod
-{
-    public override void OnInitialized()
-    {
-        SWLogger.LogInfo("TestPlugin initialized");
-    }
-}
