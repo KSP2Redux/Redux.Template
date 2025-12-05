5dd680219dfa11091eb7a0c23b01816e3c4a03d6
Lexi <lexi@ksp2redux.org>
2025-12-04 16:51:13 -0800

    update batch file to move stuff correctly

--- publicize.bat
+++ publicize.bat
@@ -1,3 +1,3 @@
 .\Utilities\NStrip\NStrip.exe -p -n -d .\Packages\KSP2_x64 -cg --cg-exclude-events --unity-non-serialized .\Packages\KSP2_x64\Assembly-CSharp.dll .\Assembly-CSharp.tmp
-rm .\Packages\KSP2_x64\Assembly-CSharp.dll
-mv .\Assembly-CSharp.tmp .\Packages\KSP2_x64\Assembly-CSharp.dll
+del .\Packages\KSP2_x64\Assembly-CSharp.dll
+move .\Assembly-CSharp.tmp .\Packages\KSP2_x64\Assembly-CSharp.dll
